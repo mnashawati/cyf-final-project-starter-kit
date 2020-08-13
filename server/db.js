d3438dbdbc5d91a7e04ce9ed75646406b8a99e81
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-08-13 22:31:04 +0100

    Update

--- server/db.js
+++ server/db.js
@@ -3,4 +3,7 @@ import { MongoClient } from "mongodb";
 const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/cyf";
 const configuration = { useNewUrlParser: true, useUnifiedTopology: true };
 
-export const getClient = () => new MongoClient(dbUrl, configuration);
+export const getClient = () => {
+	console.log("URI", dbUrl);
+	return new MongoClient(dbUrl, configuration);
+};
