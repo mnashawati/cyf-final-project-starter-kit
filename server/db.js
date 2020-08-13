3ce78f0c44e8be4aac8fdfbc568d86f8933aa1c5
mnashawati <mo.nashawati@gmail.com>
2020-08-13 23:06:41 +0100

    Loading the dotenv in the db

--- server/db.js
+++ server/db.js
@@ -1,6 +1,12 @@
 import { MongoClient } from "mongodb";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/cyf";
+console.log("dbUrl", dbUrl);
 const configuration = { useNewUrlParser: true, useUnifiedTopology: true };
 
 export const getClient = () => new MongoClient(dbUrl, configuration);
+
+
