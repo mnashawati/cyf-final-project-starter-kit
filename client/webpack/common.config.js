fbb60eec184cdd2ca9e90ed48f994b5c8864fc86
ola <olasvoice@gmail.com>
2020-08-17 15:56:16 +0200

    local host will open automaticaly

--- client/webpack/common.config.js
+++ client/webpack/common.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
+const path = require("path");
 module.exports = {
 	entry: "./client/src/index.js",
 	module: {
@@ -19,7 +19,13 @@ module.exports = {
 			},
 		],
 	},
+
 	devServer: {
+		contentBase: path.resolve("src"),
+		hot: true,
+		open: true,
+		port: 8000,
+		watchContentBase: true,
 		historyApiFallback: true,
 	},
 	plugins: [
