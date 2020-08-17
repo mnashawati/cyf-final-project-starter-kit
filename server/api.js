8e38c1813cd7490608b045e58bed712b01b043af
mnashawati <mo.nashawati@gmail.com>
2020-08-17 21:02:35 +0100

    Created POST endpoint

--- server/api.js
+++ server/api.js
@@ -3,6 +3,8 @@ import { Router } from "express";
 
 import { getClient } from "./db";
 
+import mongodb from "mongodb";
+
 const router = new Router();
 
 // router.get("/", (_, res, next) => {
@@ -30,12 +32,32 @@ router.get("/", (_, res, next) => {
 			.find()
 			.toArray(function(error, students) {
 				res.send(error || students);
-				client.close();
+				// client.close();
 			});
 	});
 });
 
-router.get("/:id", (_, res, next) => {
+// router.get("/:id", (req, res, next) => {
+// 	const client = getClient();
+// 	client.connect((err) => {
+// 		if (err) {
+// 			return next(err);
+// 		}
+// 		const db = client.db("feedback-tracker");
+// 		const collection = db.collection("students");
+
+// 		const id = new mongodb.ObjectID(req.params.id);
+// 		const queryObject = { _id: id };
+
+// 		collection
+// 			.findOne(queryObject , function(error, result) {
+// 				res.send(error || result);
+// 				// client.close();
+// 			});
+// 	});
+// });
+
+router.post("/", (req, res, next) => {
 	const client = getClient();
 	client.connect((err) => {
 		if (err) {
@@ -43,13 +65,20 @@ router.get("/:id", (_, res, next) => {
 		}
 		const db = client.db("feedback-tracker");
 		const collection = db.collection("students");
+
+		const data = req.body;
+
 		collection
-			.find()
-			.toArray(function(error, students) {
-				res.send(error || students);
-				client.close();
+			.insertOne(data, (error, result) => {
+				// if everything is not ok -> send error response (500)
+				if (error) {
+					console.log(error);
+					return res.sendStatus(500);
+				}
+				// if everything is ok -> send returned record (a bit tricky to find it...)
+				return res.status(201).send(result.ops[0]);
 			});
+		// client.close();
 	});
 });
-
 export default router;
