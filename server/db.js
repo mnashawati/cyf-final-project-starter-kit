d1927d40bd27c6da2c3f7217042c3b440c977f81
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-08-20 23:58:30 +0100

    Uncommenting local DB

--- server/db.js
+++ server/db.js
@@ -3,8 +3,7 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-const dbUrl = process.env.MONGODB_URI;
-// || "mongodb://localhost:27017/cyf";
+const dbUrl = process.env.MONGODB_URI || "mongodb://localhost:27017/cyf";
 const configuration = { useNewUrlParser: true, useUnifiedTopology: true };
 
 export const getClient = () => new MongoClient(dbUrl, configuration);
