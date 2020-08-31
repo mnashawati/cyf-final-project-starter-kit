28fe21c66ae7594d5ccf5c11a4f2d4bf3bb329f1
mnashawati <mo.nashawati@gmail.com>
2020-08-31 21:52:14 +0100

    Tidying up components

--- client/src/components/PreviousFeedback/index.js
+++ client/src/components/PreviousFeedback/index.js
@@ -21,29 +21,23 @@ const PreviousFeedback = ({ student, toggleNewFeedback }) => {
 		<>
 			<p><b>Previous Feedback</b></p>
 			<div className="previous-feedback-section">
-
-				{allFeedback.map((item, index)=>{
-
-					return (
-						<div key={index} className="previous-feedback-container">
-
-							<div className="previous-feedback-list">
-								<p className="feedback-module"><b>MODULE:</b> {item.module}</p>
-								<p className="feedback-title">{item.title}</p>
-								<p className="feedback-text">{item.text}</p>
-								<div className="date-mentor">
-									<p className="feedback-date">{item.date}</p>
-									<p className="feedback-mentor">Given by: {item.mentor}</p>
-								</div>
-							</div>
-
-							<div className="buttons">
-								<button className="previous-feedback-edit" >EDIT</button>
-								<button className="previous-feedback-delete" onClick={() => deleteFeedback(item.id)}>DELETE</button>
+				{allFeedback.map((item, index)=> (
+					<div key={index} className="previous-feedback-container">
+						<div className="previous-feedback-list">
+							<p className="feedback-module"><b>MODULE:</b> {item.module}</p>
+							<p className="feedback-title">{item.title}</p>
+							<p className="feedback-text">{item.text}</p>
+							<div className="date-mentor">
+								<p className="feedback-date">{item.date}</p>
+								<p className="feedback-mentor">Given by: {item.mentor}</p>
 							</div>
 						</div>
-					);
-				})}
+						<div className="buttons">
+							<button className="previous-feedback-edit" >EDIT</button>
+							<button className="previous-feedback-delete" onClick={() => deleteFeedback(item.id)}>DELETE</button>
+						</div>
+					</div>
+				))}
 			 </div>
 		</>
 	) : null ;
