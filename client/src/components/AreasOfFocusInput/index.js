cebf739a06ded275f7d7860223ec1af5b2ce44fc
ola <olasvoice@gmail.com>
2020-09-06 10:18:51 +0200

    areas of focus form

--- client/src/components/AreasOfFocusInput/index.js
+++ client/src/components/AreasOfFocusInput/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-for */
 import React, { useState } from "react";
 
 import "./styles.css";
@@ -6,13 +7,13 @@ const AreasOfFocusInput = ({ addNewArea }) => {
 	const [area, setArea] = useState({
 		message: "",
 		level: "",
-	}); // { message: "" level: "" id: "" }
+	});
 
 	const handleSubmit = (e) => {
 		if (!area.message) {
 			return alert("Please add an area");
 		} else if (!area.level) {
-			return alert("Please select a level");
+			return  alert("Please select a level");
 		}
 		e.preventDefault();
 		addNewArea(area);
@@ -25,57 +26,73 @@ const AreasOfFocusInput = ({ addNewArea }) => {
 	};
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<div>
-				<p className="areas-input-label">
+		<div className="area-form-section">
+			<form onSubmit={handleSubmit}>
+				<div>
+					<p className="areas-input-label">
           Add area of focus:
-				</p>
-			</div>
-			<div className="input-lights-add-button-container">
-				<div className="area-input-section">
-					<input
-						className="areas-input"
-						type="text"
-						name="message"
-						placeholder="eg. CSS, useEffect Hook..."
-						maxLength={15}
-						onChange={handleChange}
-					/>
+					</p>
 				</div>
-				<div className="lights-section">
-					<input
-						className="send-input-red"
-						type="radio"
-						id="male"
-						name="level"
-						value="To work on"
-						onChange={handleChange}
-					></input>
-					<input
-						className="send-input-red"
-						type="radio"
-						id="male"
-						name="level"
-						value="Okay at"
-						onChange={handleChange}
-					></input>
-					<input
-						className="send-input-red"
-						type="radio"
-						id="male"
-						name="level"
-						value="Good at"
-						onChange={handleChange}
-					></input>
-				</div>
-				<div className="add-area-button-section">
-					<div className="area-display">{area.level}</div>
-					<div className="buton-wrapper">
-						<button className="add-area-button">Add</button>
+				<div className="input-lights-add-button-container">
+					<div className="area-input-section">
+						<input
+							className="areas-input"
+							type="text"
+							name="message"
+							maxLength={15}
+							onChange={handleChange}
+						/>
+					</div>
+					<div className="lights-section">
+						<div>
+							<label className="radio-circle-section-red">
+								<input
+									className="send-input-red"
+									type="radio"
+									id="male"
+									name="level"
+									value="To work on"
+									onChange={handleChange}
+								></input>
+								<span className="circle-red"></span>
+							</label>
+						</div>
+						<div>
+							<label className="radio-circle-section-yellow">
+								<input
+									className="send-input-red"
+									type="radio"
+									id="male"
+									name="level"
+									value="Okay at"
+									onChange={handleChange}
+								></input>
+								<span className="circle-yellow"></span>
+							</label>
+						</div>
+						<div>
+							<label className="radio-circle-section-green">
+								<input
+									className="send-input-green"
+									type="radio"
+									id="male"
+									name="level"
+									value="Good at"
+									onChange={handleChange}
+								></input>
+								<span className="circle-green"></span>
+							</label>
+						</div>
+					</div>
+					<div className="add-area-button-section">
+						<div className="area-display">{area.level}</div>
+						<div className="buton-wrapper">
+							<button className="add-area-button">Post area</button>
+						</div>
 					</div>
 				</div>
-			</div>
-		</form>
+			</form>
+		</div>
 	);
 };
 
