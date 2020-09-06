7bfd5904264158562a93eebdb4446c3ff10d0ea7
mnashawati <mo.nashawati@gmail.com>
2020-09-06 02:15:14 +0100

    Amended the path on the Navbar to lead to regions

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import "./styles.css";
 
 const Navbar = () => {
 	return <div className="navbar-container">
 		<Link
 			className="navbar-route-link"
-			to={{ pathname: "/students" }}
+			to={{ pathname: "/regions" }}
 		><h3>Feedback Tracker</h3>
 		</Link>
 	</div>;
