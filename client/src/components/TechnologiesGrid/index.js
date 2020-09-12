3621a9008616fa436491cdcfebcc33e0fb697b15
mnashawati <mo.nashawati@gmail.com>
2020-09-12 20:30:42 +0100

    Styling technologies grid

--- client/src/components/TechnologiesGrid/index.js
+++ client/src/components/TechnologiesGrid/index.js
@@ -5,12 +5,32 @@ import "../RegionsGrid/grid.css";
 const TechnologiesGrid = () => {
 	return (
 		<div className="technologies-container">
-			<div className="html-card"></div>
-			<div className="css-card"></div>
-			<div className="js-card"></div>
-			<div className="react-card"></div>
-			<div className="nodejs-card"></div>
-			<div className="mongodb-card"></div>
+			<div className="row">
+				<div className="col-12 sm-col-6 lg-col-4">
+					<div className="html-card"></div>
+
+				</div>
+				<div className="col-12 sm-col-6 lg-col-4">
+					<div className="css-card"></div>
+
+				</div>
+				<div className="col-12 sm-col-6 lg-col-4">
+					<div className="js-card"></div>
+
+				</div>
+				<div className="col-12 sm-col-6 lg-col-4">
+					<div className="react-card"></div>
+
+				</div>
+				<div className="col-12 sm-col-6 lg-col-4">
+					<div className="nodejs-card"></div>
+
+				</div>
+				<div className="col-12 sm-col-6 lg-col-4">
+					<div className="mongodb-card"></div>
+
+				</div>
+			</div>
 		</div>
 	);
 };
