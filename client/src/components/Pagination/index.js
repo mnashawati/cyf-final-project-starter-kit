8fbc38b880a0c800fcec9fcf80d594f276b053c9
ola <olasvoice@gmail.com>
2020-09-15 15:58:14 +0200

    pagination component added

---
+++ client/src/components/Pagination/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./styles.css";
+
+const Pagination = ({ totalStudents, studentsPerPage, paginate }) => {
+	const pageNumbers = [];
+
+	const numberOfPages = totalStudents / studentsPerPage;
+	for (let i = 1; i <= Math.ceil(numberOfPages) ;i++) {
+		pageNumbers.push(i);
+	}
+
+	return (
+		<div className="pagination-">
+			<ul className="pagination">
+				{pageNumbers.map((number) => (
+					<li  key={number} className="page-item">
+						<a onClick={(e) => {
+							e.preventDefault(),
+							paginate(number);
+						}}
+						href='!#'
+						className="page-link">
+							{number}
+						</a>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
+
+Pagination.propTypes = {
+	totalStudents: PropTypes.number.isRequired,
+	studentsPerPage: PropTypes.number.isRequired,
+	paginate: PropTypes.func.isRequired,
+};
+
+export default Pagination;
