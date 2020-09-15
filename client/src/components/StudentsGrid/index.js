b4f133f5b0f1d4b5fda1f816e030581137fa934d
ola <olasvoice@gmail.com>
2020-09-15 20:23:46 +0200

    state for pagination added

--- client/src/components/StudentsGrid/index.js
+++ client/src/components/StudentsGrid/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import StudentCard from "../StudentCard/index.js";
+import Pagination from "../Pagination/index.js";
 import "./styles.css";
 import Navbar from "../Navbar/index.js";
 
@@ -8,22 +9,34 @@ const StudentsGrid = ( ) => {
 
 	const params = useParams();
 	const [students, setStudents] = useState([]);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [studentsPerPage] = useState(6);
+
 
 	useEffect(() => {
-		fetch("/api/students")
-			.then((res) => res.json())
-			.then((data) => setStudents(data.filter((student) => student.city == params.regionName)))
-			.catch((err) => console.log(err));
-	},[]);
 
-	return  (
+		setTimeout(function () {
+			fetch("/api/students")
+				.then((res) => res.json())
+				.then((data) => setStudents(data.filter((student) => student.city == params.regionName)))
+				.catch((err) => console.log(err));
+	}, []);
+
+	const indexOfLastStudent = currentPage * studentsPerPage; // 6
+	const indexOfFirstStudent = indexOfLastStudent - studentsPerPage; // 6-6=0
+	const currentStudent = students.slice(indexOfFirstStudent, indexOfLastStudent);// students.slice(0,6)
+
+	const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+	return (
 		<>
 			<Navbar />
 			<div className="students-cards-container row">
-				{ students && students.map((student, index) => (
+				{currentStudent && currentStudent.map((student, index) => (
 					<StudentCard student={student} key={index} />
 				))}
 			</div>
+			<Pagination studentsPerPage={studentsPerPage} totalStudents={students.length} paginate={paginate} />
 		</>
 	);
 };
