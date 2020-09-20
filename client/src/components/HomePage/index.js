4a12abfa616c4561d57e298c0bd28675a463e370
mnashawati <mo.nashawati@gmail.com>
2020-09-20 21:54:51 +0100

    Homepage Styling

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import Navbar from "../Navbar/index";
 import HomepageHeroSection from "../HomepageHeroSection";
 import Footer from "../Footer/index";
+import "./styles.css";
 
 const HomePage = () => {
 	return (
-		<div>
+		<div className="homepage-container">
 			<Navbar />
 			<HomepageHeroSection />
 			<Footer />
