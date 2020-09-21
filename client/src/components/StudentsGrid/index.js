8a96c1e8997db14d20536c5d07d84770ac95d12c
mnashawati <mo.nashawati@gmail.com>
2020-09-22 00:21:29 +0100

    Added filter students by class number

--- client/src/components/StudentsGrid/index.js
+++ client/src/components/StudentsGrid/index.js
@@ -4,6 +4,8 @@ import StudentCard from "../StudentCard/index.js";
 import Pagination from "../Pagination/index.js";
 import Navbar from "../Navbar/index.js";
 import Footer from "../Footer/index";
+import getFilteringData from "../../helperFunctions/getFilteringData";
+import "./styles.css";
 
 const StudentsGrid = ( ) => {
 
@@ -15,7 +17,12 @@ const StudentsGrid = ( ) => {
 	useEffect(() => {
 		fetch("/api/students")
 			.then((res) => res.json())
-			.then((data) => setStudents(data.filter((student) => student.city == params.regionName)))
+			.then((data) => setStudents(data.filter((student) => student.city == params.regionName)
+				.sort((a, b) => {
+					let firstName = a.name.toLowerCase();
+					let secondName = b.name.toLowerCase();
+					return firstName > secondName ? 1 : firstName < secondName ? -1 : 0;
+				})))
 			.catch((err) => console.log(err));
 	}, []);
 
@@ -23,11 +30,18 @@ const StudentsGrid = ( ) => {
 	const indexOfFirstStudent = indexOfLastStudent - studentsPerPage; // 6-6=0
 	const currentStudents = students.slice(indexOfFirstStudent, indexOfLastStudent);// students.slice(0,6)
 
-
 	return (
 		<>
-			<Navbar />
-			<div className="container">
+			<Navbar linkClassName={"back-to-all-regions"} linkPathName="/regions" linkContent={"Back to all regions"} />
+			<div className="container students-grid-container">
+				<div className="filter-by-class-container row">
+					<h2>Filter by class number: </h2>
+					<select className="select-class-number">
+						<option>Select a class</option>
+						{getFilteringData(students, "class")
+							.map((el, index) => <option key={index}>Class: {el}</option>)}
+					</select>
+				</div>
 				<div className="students-cards-container row">
 					{currentStudents && currentStudents.map((student, index) => (
 						<StudentCard student={student} key={index} />
