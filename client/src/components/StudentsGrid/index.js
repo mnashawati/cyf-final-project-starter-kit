4f6ebc15db37ac2c5eb27d77612e70f4fdade25c
Tayfun Aydogan <40833117+tayfuntayfun@users.noreply.github.com>
2020-09-23 14:12:11 +0100

    Filter by Class Number

--- client/src/components/StudentsGrid/index.js
+++ client/src/components/StudentsGrid/index.js
@@ -13,6 +13,7 @@ const StudentsGrid = ( ) => {
 	const [students, setStudents] = useState([]);
 	const [currentPage, setCurrentPage] = useState(1);
 	const [studentsPerPage] = useState(6);
+	const [currentClass, setCurrentClass] = useState(6)
 
 	useEffect(() => {
 		fetch("/api/students")
@@ -28,30 +29,48 @@ const StudentsGrid = ( ) => {
 
 	const indexOfLastStudent = currentPage * studentsPerPage; // 6
 	const indexOfFirstStudent = indexOfLastStudent - studentsPerPage; // 6-6=0
-	const currentStudents = students.slice(indexOfFirstStudent, indexOfLastStudent);// students.slice(0,6)
+	const currentStudents = students.filter((student) => student.class === currentClass).slice(indexOfFirstStudent, indexOfLastStudent);// students.slice(0,6)
 
 	return (
-		<>
-			<Navbar linkClassName={"back-to-all-regions"} linkPathName="/regions" linkContent={"Back to all regions"} />
-			<div className="container students-grid-container">
-				<div className="filter-by-class-container row">
-					<h2>Filter by class number: </h2>
-					<select className="select-class-number">
-						<option>Select a class</option>
-						{getFilteringData(students, "class")
-							.map((el, index) => <option key={index}>Class: {el}</option>)}
-					</select>
-				</div>
-				<div className="students-cards-container row">
-					{currentStudents && currentStudents.map((student, index) => (
-						<StudentCard student={student} key={index} />
-					))}
-				</div>
-			</div>
-			<Pagination itemsPerPage={studentsPerPage} totalItems={students.length} setCurrentPage={setCurrentPage} />
-			<Footer />
-		</>
-	);
+    <>
+      <Navbar
+        linkClassName={"back-to-all-regions"}
+        linkPathName="/regions"
+        linkContent={"Back to all regions"}
+      />
+      <div className="container students-grid-container">
+        <div className="filter-by-class-container row">
+          <h2>Filter by class number: </h2>
+          <select
+            value={`Class:" ${currentClass}`}
+            className="select-class-number"
+            onChange={(e) => setCurrentClass(e.target.value)}
+          >
+            <option>Select a class</option>
+            {getFilteringData(students, "class")
+              .sort()
+              .map((el, index) => (
+                <option key={index} value={el}>
+                  Class: {el}
+                </option>
+              ))}
+          </select>
+        </div>
+        <div className="students-cards-container row">
+          {currentStudents &&
+            currentStudents.map((student, index) => (
+              <StudentCard student={student} key={index} />
+            ))}
+        </div>
+      </div>
+      <Pagination
+        itemsPerPage={studentsPerPage}
+        totalItems={students.length}
+        setCurrentPage={setCurrentPage}
+      />
+      <Footer />
+    </>
+  );
 };
 
 export default StudentsGrid;
