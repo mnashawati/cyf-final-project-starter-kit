09fa208912993d93e434e7094a8435e410f7c978
ola <olasvoice@gmail.com>
2020-09-25 12:18:41 +0200

    unused deleted

--- client/src/components/StudentProfile/index.js
+++ client/src/components/StudentProfile/index.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
-import backArrow from "../../assets/image-two.png";
 import Highlights from "../Highlights/index.js";
 import AllFeedback from "../AllFeedback/index.js";
 import "./styles.css";
